f46590bf046be3021e88a250f228bb305792492a
KenichiroArai <sammyleejunior@gmail.com>
2025-11-03 22:23:50 +0900

    #6 package-info.javaの作成。
    (ドキュメントの整備 #6)

---
+++ src/main/java/kmg/tool/gui/cmn/presentation/ui/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * KMGツールGUI共通のUI層パッケージ<br>
+ * <p>
+ * KMGツールGUIの共通UI（ユーザーインターフェース）層の機能を格納するパッケージです。<br>
+ * 複数のツールで共通して利用されるUI関連の機能が実装されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.gui.cmn.presentation.ui;
+
---
+++ src/main/java/kmg/tool/gui/is/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * 識別子作成ツールGUIのパッケージ<br>
+ * <p>
+ * 識別子（Identifier String）作成ツールのGUI機能を格納するパッケージです。<br>
+ * 識別子の生成や管理に関するユーザーインターフェースが実装されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.gui.is;
+
